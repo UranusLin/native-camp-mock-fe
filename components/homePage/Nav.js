b880934ff241bffc2b4b040b525c8c914d3525b0
MorrisLin <aka611511@gmail.com>
2021-09-20 23:15:38 +0800

    feat: finish header

--- components/homePage/Nav.js
+++ components/homePage/Nav.js
@@ -1,9 +1,8 @@
-import React from 'react'
 
 function Nav() {
     return (
         <div>
-            
+
         </div>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,31 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        nativeCamp: {
+          header: {
+            bg: "#112844",
+            signup: '#F1890E',
+            signupHov: '#ffa436',
+            login: "#03c9a9",
+            loginHov: "#22d6b8",
+          },
+        }
+      },
+      height: {
+        logo: "30px",
+        header: "83px"
+      },
+      fontSize: {
+        headerLogo: ['2.0px', '1.2rem'],
+      }
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    // require('tailwind-scrollbar-hide')
+  ],
 }
